c17e366a8ab83ec11577d890e8d5ed9177fd7d25
pundit <pundit@pundit.pundit>
2025-06-21 19:35:01 +0300

    Initial clean commit without node_modules

--- src/scenes/Payout/index.jsx
+++ src/scenes/Payout/index.jsx
@@ -31,7 +31,7 @@ import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import CancelIcon from "@mui/icons-material/Cancel";
 import HourglassBottomIcon from "@mui/icons-material/HourglassBottom";
 
-import { usePayouts } from "@/hooks/payouts"; // Ensure this returns the correct data shape
+import usePayout from '../../hooks/payouts'; 
 
 const Payout = () => {
   const { payouts } = usePayouts();
